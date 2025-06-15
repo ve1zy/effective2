a7b94b400fd049d90b1d21ec744ec78940428f12
Pavel Zhukov Alekseevich <zhukowgg@gmail.com>
2025-06-15 18:12:46 +0600

    add lab

--- app.js
+++ app.js
@@ -1,41 +1,31 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const mongoDB = 'mongodb://127.0.0.1/local_library';
+mongoose.connect(mongoDB);
 
-var app = express();
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-// view engine setup
+require('./models/author');
+require('./models/book');
+require('./models/genre');
+require('./models/bookinstance');
+
+const app = express();
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,64 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const Book = require('../models/book');
+const Author = require('../models/author');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', async (req, res) => {
+  try {
+    const books = await Book.find().populate('author').populate('genre').exec();
+    res.render('index', { title: 'Local Library', books });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Server Error');
+  }
+});
+
+router.get('/author/:id', async (req, res) => {
+  try {
+    const author = await Author.findById(req.params.id).exec();
+
+    if (!author) {
+      return res.status(404).render('error', {
+        message: 'Author not found',
+        error: { status: 404, stack: 'Author with the specified ID does not exist.' },
+      });
+    }
+
+    const books = await Book.find({ author: req.params.id }, 'title summary').exec();
+
+    res.render('author_detail', { title: 'Author Details', author, books });
+  } catch (err) {
+    console.error(err);
+    res.status(500).render('error', {
+      message: 'Server Error',
+      error: { status: 500, stack: err.stack },
+    });
+  }
+});
+
+router.get('/book/:id', async (req, res) => {
+  try {
+    const book = await Book.findById(req.params.id)
+      .populate('author')
+      .populate('genre')
+      .populate('bookinstances') 
+      .exec();
+
+    if (!book) {
+      return res.status(404).render('error', {
+        message: 'Book not found',
+        error: { status: 404, stack: 'Book with the specified ID does not exist.' },
+      });
+    }
+
+    res.render('book_detail', { title: 'Book Details', book });
+  } catch (err) {
+    console.error(err);
+    res.status(500).render('error', {
+      message: 'Server Error',
+      error: { status: 500, stack: err.stack },
+    });
+  }
 });
 
 module.exports = router;
